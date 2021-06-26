305ed653a9096153969017059c2978bdf31684b7
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-06-26 23:57:05 +0900

    refactor: assure that conditional branch sees grep result

--- oyainput-starter.sh
+++ oyainput-starter.sh
@@ -3,13 +3,13 @@ script=$(basename $0) # oyainput-starter.sh
 echo "${script}: Start script..."
 while true
 do
-  result=$(ps ax | grep -v -e grep -e ${script} | grep -q oyainput)
+  ps ax | grep -v -e grep -e ${script} | grep -q oyainput || result=$?
   # プロセスを監視し、oyainput が起動していなければ起動する
-  if [ $? -ne 0 ]; then
+  if [ ! ${result} = "0" ]; then
     echo "oyainput is not running. Start oyainput..."
     oyainput
   else
-    echo "oyainput is running."
+    echo "oyainput process is detected."
   fi
   sleep 10
 done
