b87461a662ec33d023abbc7b41d01fa0deb2682c
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2021-06-26 23:22:57 +0900

    chore: output current status

--- oyainput-starter.sh
+++ oyainput-starter.sh
@@ -1,6 +1,15 @@
+script=$(basename $0) # oyainput-starter.sh
+
+echo "${script}: Start script..."
 while true
 do
+  result=$(ps ax | grep -v -e grep -e ${script} | grep -q oyainput)
   # プロセスを監視し、oyainput が起動していなければ起動する
-  ps ax | grep -v grep | grep -q oyainput || oyainput
+  if [ $? -ne 0 ]; then
+    echo "oyainput is not running. Start oyainput..."
+    oyainput
+  else
+    echo "oyainput is running."
+  fi
   sleep 10
 done
